11fc6cd67600b98b90b8b890fcbe94ecc9552380
leanfolks <leanfolks@leanfolks.local>
2025-01-30 12:54:48 +0530

    newCertificateImages

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,52 @@
-import React, { useState } from 'react';
-import Papa from 'papaparse';
-import './app.css';
+import React, { useState } from "react";
+import Papa from "papaparse";
+import "./app.css";
 
 function App() {
   const [data, setData] = useState([]);
-  const [bibNumber, setBibNumber] = useState('');
+  const [bibNumber, setBibNumber] = useState("");
   const [selectedEntry, setSelectedEntry] = useState(null);
   const [csvUploaded, setCsvUploaded] = useState(false);
 
   const getCertificateImage = (bib) => {
-    if (bib >= 2000 && bib <= 2999) {
+    if (String(bib).startsWith("D")) {
       return {
-        image: 'https://i.ibb.co/mJLyWGy/2KM.jpg',
-        namePos: { x: '68%', y: '28%' },
-        bibPos: { x: '68%', y: '21%' },
-        categoryPos: { x: '68%', y: '10%' },
-        nameColor: '#FF2929',
-        bibColor: '#FF2929',
-        categoryColor: 'orange',
+        image: "https://i.ibb.co/chmC3Ck2/2KM.jpg",
+        namePos: { x: "65%", y: "28%" },
+        bibPos: { x: "65%", y: "21%" },
+        nameColor: "white",
+        bibColor: "white",
       };
     }
     if (bib >= 10000 && bib <= 10999) {
       return {
-        image: 'https://i.ibb.co/kGFgfhB/10KM.jpg',
-        namePos: { x: '67%', y: '26%' },
-        bibPos: { x: '67%', y: '19%' },
-        categoryPos: { x: '', y: '' },
-        nameColor: 'white',
-        bibColor: 'orange',
+        image: "https://i.ibb.co/4ZKJ7Hdc/10KM.jpg",
+        namePos: { x: "65%", y: "29%" },
+        bibPos: { x: "65%", y: "22%" },
+        nameColor: "white",
+        bibColor: "white",
       };
     }
     if (bib >= 21000 && bib <= 21999) {
       return {
-        image: 'https://i.ibb.co/JswXf3D/21KM.jpg',
-        namePos: { x: '67.5%', y: '25%' },
-        bibPos: { x: '67.5%', y: '18%' },
-        categoryPos: { x: '', y: '' },
-        nameColor: 'white',
-        bibColor: 'white',
+        image: "https://i.ibb.co/gMJMRW08/HALF-MARATHON.jpg",
+        namePos: { x: "70%", y: "26%" },
+        bibPos: { x: "70%", y: "19%" },
+        nameColor: "white",
+        bibColor: "white",
       };
     }
     if (bib >= 50000) {
       return {
-        image: 'https://i.ibb.co/PG9xv1B/5KM.jpg',
-        namePos: { x: '67.5%', y: '25%' },
-        bibPos: { x: '67.5%', y: '18%' },
-        categoryPos: { x: '%', y: '' },
-        nameColor: 'white',
-        bibColor: 'white',
+        image: "https://i.ibb.co/fVrPd69m/5KM.jpg",
+        namePos: { x: "70%", y: "26%" },
+        bibPos: { x: "70%", y: "19%" },
+        nameColor: "white",
+        bibColor: "white",
       };
     }
-    return {
-      image: '',
-      namePos: { x: '50%', y: '45%' },
-      bibPos: { x: '50%', y: '45%' },
-      categoryPos: { x: '50%', y: '50%' },
-      nameColor: 'black',
-      bibColor: 'black',
-      categoryColor: 'gray',
-    };
+
+    throw new Error("Error: Check your bib number");
   };
 
   const handleFileUpload = (event) => {
@@ -77,17 +65,10 @@ function App() {
     const entry = data.find((row) => row.bibNumber == bibNumber);
     setSelectedEntry(entry || null);
     if (entry) {
-      const {
-        image,
-        namePos,
-        bibPos,
-        categoryPos,
-        nameColor,
-        bibColor,
-        categoryColor,
-      } = getCertificateImage(entry.bibNumber);
+      const { image, namePos, bibPos, nameColor, bibColor } =
+        getCertificateImage(entry.bibNumber);
 
-      let certificateWindow = window.open('', 'certificateWindow');
+      let certificateWindow = window.open("", "certificateWindow");
 
       if (certificateWindow && !certificateWindow.closed) {
         certificateWindow.document.body.innerHTML = `
@@ -116,7 +97,7 @@ function App() {
                   top: ${namePos.y};
                   left: ${namePos.x};
                   transform: translate(-50%, -50%);
-                  font-size: 1.5rem;
+                  font-size: 2.0rem;
                   font-family: 'Times New Roman', Times, serif; 
                   font-weight: normal;
                   color: ${nameColor}; /* Dynamic color for name */
@@ -126,19 +107,10 @@ function App() {
                   top: ${bibPos.y};
                   left: ${bibPos.x};
                   transform: translate(-50%, -50%);
-                  font-size: 3.0rem;
+                  font-size: 6.0rem;
                   font-family: 'Times New Roman', Times, serif;
                   color: ${bibColor}; /* Dynamic color for bib number */
                 }
-                .category {
-                  position: absolute;
-                  top: ${categoryPos.y};
-                  left: ${categoryPos.x};
-                  transform: translate(-50%, -50%);
-                  font-size: 1.5rem;
-                  font-family: 'Times New Roman', Times, serif;
-                  color: ${categoryColor};
-                   font-weight: bold; /* Makes the text bold */}
 
               </style>
             </head>
@@ -147,72 +119,74 @@ function App() {
                 <img src="${image}" alt="Certificate" style="width: 100%; height: 100%; object-fit: contain;" />
                 <div class="name">${entry.firstName}</div>
                 <div class="bib">${entry.bibNumber}</div>
-                <div class="category">${entry.category || 'N/A'}</div>
               </div>
             </body>
           </html>
         `;
         certificateWindow.document.close();
       } else {
-        alert('Unable to open or update the certificate window.');
+        alert("Unable to open or update the certificate window.");
       }
     } else {
-      alert('Bib number not available');
+      alert("Bib number not available");
     }
   };
 
   return (
     <div
-      className="App h-full w-auto bg-cover bg-center flex justify-center items-center px-6 py-8 relative"
+      className='App h-full w-full bg-cover bg-center bg-no-repeat flex justify-center items-center px-6 py-8 relative'
       style={{
         backgroundImage: "url('https://i.ibb.co/9p0jf8s/background.jpg')",
+        backgroundSize: "cover", 
+        backgroundPosition: "center", 
+        width: "100vw", 
+        height: "100vh",
+        objectFit: "cover", 
       }}
     >
-      <div className="bg-black bg-opacity-80 p-8 rounded-lg shadow-lg w-[90%] max-w-xl h-[80vh] flex flex-col justify-center items-center text-center">
+      <div className='bg-black bg-opacity-80 p-8 rounded-lg shadow-lg w-[90%] max-w-xl h-[80vh] flex flex-col justify-center items-center text-center'>
         <img
-          src="https://i.ibb.co/ZcpbbSj/nova-logo.png"
-          alt="Nova Logo"
-          className="w-96 h-auto mb-6 mx-auto"
+          src='https://i.ibb.co/ZcpbbSj/nova-logo.png'
+          alt='Nova Logo'
+          className='w-96 h-auto mb-6 mx-auto'
         />
-        <div className="p-8 rounded-lg shadow-lg w-full max-w-md text-center">
-          <h1 className="font-bold text-white mb-6 text-3xl">Enter Bib Number</h1>
-          <div className="flex justify-center mb-6">
-          <input
-  type="text"
-  placeholder="Enter Bib Number"
-  value={bibNumber}
-  onChange={(e) => setBibNumber(e.target.value)}
-  className="p-4 w-[80%] border border-gray-300 rounded-lg mb-4 text-lg font-bold text-center focus:outline-none focus:ring-2 focus:ring-blue-500"
-/>
-
+        <div className='p-8 rounded-lg shadow-lg w-full max-w-md text-center'>
+          <h1 className='text-white mb-6 text-3xl'>Enter Bib Number</h1>
+          <div className='flex justify-center mb-6'>
+            <input
+              type='text'
+              placeholder=''
+              value={bibNumber}
+              onChange={(e) => setBibNumber(e.target.value)}
+              className='p-4 w-[80%] border border-gray-300 rounded-lg mb-4 text-3xl font-bold text-center focus:outline-none focus:ring-2 focus:ring-blue-500'
+            />
           </div>
           <button
             onClick={handleSearch}
-            className="bg-[#007BFF] text-white px-6 py-3 rounded-lg shadow-md hover:bg-[#0056b3] transition-all duration-200 w-60"
+            className='bg-[#007BFF] text-white px-6 py-3 rounded-lg shadow-md hover:bg-[#0056b3] transition-all duration-200 w-60'
           >
             Search
           </button>
         </div>
       </div>
-  
-      <div className="absolute top-4 right-4">
+
+      <div className='absolute top-4 right-4'>
         <input
-          type="file"
-          accept=".csv"
+          type='file'
+          accept='.csv'
           onChange={handleFileUpload}
-          className="hidden"
-          id="fileUpload"
+          className='hidden'
+          id='fileUpload'
         />
         <label
-          htmlFor="fileUpload"
-          className="bg-[#007BFF] text-white px-6 py-3 rounded-lg shadow-md hover:bg-blue-600 cursor-pointer transition-all duration-200"
+          htmlFor='fileUpload'
+          className='bg-[#007BFF] text-white px-6 py-3 rounded-lg shadow-md hover:bg-blue-600 cursor-pointer transition-all duration-200'
         >
-          {csvUploaded ? 'CSV Uploaded' : 'Upload CSV'}
+          {csvUploaded ? "CSV Uploaded" : "Upload CSV"}
         </label>
       </div>
     </div>
   );
-  
 }
 
 export default App;
